ce9cacb80db09a8bae21518fba0691d313b44548
Rob Goelz <RobGoelz@gmail.com>
2018-05-18 17:13:33 -0400

    update design.js to add color functionality

--- Pixel Art Maker - DOM/designs.js
+++ Pixel Art Maker - DOM/designs.js
@@ -1,12 +1,12 @@
 let table = document.querySelector('table');
 let sizePicker = document.querySelector('#sizePicker');
-
+let pixelCanvas = document.querySelector('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener('submit', function makeGrid (event) {
   // Your code goes here!
-  // if ('tr') {
-  //   document.querySelector('table').removeElement();
-  // }
+  if (document.querySelector('tr')) {
+    document.querySelector('tr').remove();
+  }
 
   // Select size input
   const rows = document.querySelector('#inputHeight').value;
@@ -20,9 +20,13 @@ sizePicker.addEventListener('submit', function makeGrid (event) {
   for (let i = 1; i <= rows; i++) {
     table.appendChild(tRow);
     for (let j = 1; j <= cols; j++) {
-      document.querySelector('tr').appendChild(tData);
+      table.querySelector('tr').appendChild(tData);
     }
   }
   event.preventDefault();
 });
 // Select color input
+pixelCanvas.addEventListener('click', function () {
+  let paint = document.querySelector('#colorPicker').value;
+  document.querySelector('td').style.color = paint;
+});
