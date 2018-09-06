d1c5d4549ecd17185de002a10ed365fba55a3e4a
Rob Goelz <RobGoelz@gmail.com>
2018-09-06 17:51:46 -0400

    fix: add char-horn-girl.png to engine.js to loaded
    the char-horn-girl image was not loaded in the game engine and was
    causing Dev Tools to throw an error, added it there and fixed issue

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -24,21 +24,23 @@ Enemy.prototype.render = function () {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-function Player () {
+var Player = function () {
   this.sprite = 'images/char-horn-girl.png';
-  this.update = function () {
+  this.update = function (dt) {
   };
   this.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
   };
-  this.handleInput = function () {
+  this.handleInput = function (dt) {
   };
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 var player = new Player();
+var allEnemies = [new Enemy()];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
