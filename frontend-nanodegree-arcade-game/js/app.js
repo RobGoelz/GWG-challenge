cc8e1593c4db8439f7b9f7b5ae1859029455e582
Rob Goelz <RobGoelz@gmail.com>
2018-09-06 17:37:06 -0400

    feat: add Hero class (player object)

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -24,11 +24,21 @@ Enemy.prototype.render = function () {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+function Player () {
+  this.sprite = 'images/char-horn-girl.png';
+  this.update = function () {
+  };
+  this.render = function () {
+  };
+  this.handleInput = function () {
+  };
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function (e) {
