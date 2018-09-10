cbcd74cc7c2e4d0c3f5dc492f294fd90d034a4aa
Rob Goelz <RobGoelz@gmail.com>
2018-09-10 14:50:35 -0400

    feat: finalize testing of The menu test suite in feedreader.js

--- frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader/jasmine/spec/feedreader.js
@@ -36,23 +36,23 @@ $(function() {
         expect(allFeeds[i].name).not.toBe(0);
       }
     });
-
   });
 
-  /* TODO: Write a new test suite named "The menu" */
+  describe('The menu', function () {
 
-  /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+    it('has menu element hidden', function () {
+      expect($('.menu-hidden')).toBeDefined(true);
+    });
 
-  /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
 
+    it('menu changes visibility on click', function (done) {
+      $('.menu-icon-link').trigger('click');
+      expect($('body').hasClass('menu-hidden')).toBe(false);
+      $('.menu-icon-link').trigger('click');
+      expect($('body').hasClass('menu-hidden')).toBe(true);
+    });
+
+  });
   /* TODO: Write a new test suite named "Initial Entries" */
 
   /* TODO: Write a test that ensures when the loadFeed
