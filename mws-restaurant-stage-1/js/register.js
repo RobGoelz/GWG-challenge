c3cdbb8d2e9609adf37b12234d9ce2236f7f71f9
Rob Goelz <RobGoelz@gmail.com>
2018-10-09 14:13:23 -0400

    feat: update project with all service worker content and attributions

--- mws-restaurant-stage-1/js/register.js
+++ mws-restaurant-stage-1/js/register.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(function reg () {
-      console.log(`Service Worker registration succeeded: ${reg.scope}`);
+      console.log('Service Worker registration succeeded!');
     })
     .catch(function (error) {
       console.log(error);
