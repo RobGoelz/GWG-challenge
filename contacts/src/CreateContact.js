01bdec9c570dc64073815e27a37458c08310eb4c
Rob Goelz <RobGoelz@gmail.com>
2018-10-12 17:13:12 -0400

    feat: add CreateContact component that's displayed based on state

---
+++ contacts/src/CreateContact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class CreateContact extends Component {
+
+  render() {
+   return (
+    <div>
+      {/* <button className="add-contact"> */}
+      Create Contact!
+      {/* </button> */}
+    </div>
+   )
+  }
+}
+
+export default CreateContact
