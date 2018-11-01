47edfedc31f29981f0b727c15e20df88605f5af5
Rob Goelz <RobGoelz@gmail.com>
2018-11-01 15:54:34 -0400

    feat: update to add toggle drawer for filtering, call Filter component

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import locations from './data/venues.json'
 import MapDisplay from './components/MapDisplay'
-import Options from './components/Options'
+import Filter from './components/Filter'
 
 class App extends Component {
   state = {
@@ -12,17 +12,52 @@ class App extends Component {
     all: locations
   }
 
+  styles = {
+    menuButton: {
+      marginLeft: 10,
+      marginRight: 20,
+      position: 'absolute',
+      left: 10,
+      top: 10,
+      background: '#f2f2f2',
+      padding: 10
+    },
+    hide: {
+      display: 'none'
+    },
+    header: {
+      marginTop: "0px"
+    }
+  }
+
+  toggleDrawer = () => {
+    // toggles value controlling drawer display
+    this.setState({
+      open: !this.state.open
+    });
+  }
+
   render() {
     return (
       <div className="App">
+        <div>
+          <button onClick={this.toggleDrawer}
+          style={this.styles.menuButton}>
+            <i className="fa fa-bars"></i>
+          </button>
         <h1 className="banner">
           Morgantown Spots!
         </h1>
+        </div>
         <MapDisplay
           lat={this.state.lat}
           lon={this.state.lon}
           zoom={this.state.zoom}
           locations={this.state.all}/>
+        <Filter
+          locations={this.state.all}
+          open={this.state.open}
+          toggleDrawer={this.toggleDrawer}/>
       </div>
     );
   }
